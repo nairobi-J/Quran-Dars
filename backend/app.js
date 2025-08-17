6b34241ad9116c38e576a5bfa4f8fc5223acbc34
nairobi-j <jerinusrat001@gmail.com>
2025-08-18 02:59:48 +0600

    try

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Your Next.js frontend
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST']
 }));
 
